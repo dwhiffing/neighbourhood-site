54d389b399367d0a42eea16780e040190cfc79ee
Daniel Whiffing <danielwhiffing@gmail.com>
2021-04-26 12:21:04 -0400

    Fixes

--- src/useIsMobile.js
+++ src/useIsMobile.js
@@ -1,12 +1,16 @@
 import { useState, useEffect } from 'react'
 
 export const useIsMobile = () => {
-  const [width, setWidth] = useState(window ? window.innerWidth : 0)
+  const [width, setWidth] = useState(
+    typeof document !== 'undefined' ? window.innerWidth : 0,
+  )
 
   useEffect(() => {
-    const handleResize = () => setWidth(window.innerWidth)
-    window.addEventListener('resize', handleResize)
-    return () => window.removeEventListener('resize', handleResize)
+    if (typeof document !== 'undefined') {
+      const handleResize = () => setWidth(window.innerWidth)
+      window.addEventListener('resize', handleResize)
+      return () => window.removeEventListener('resize', handleResize)
+    }
   }, [])
 
   return width <= 900
