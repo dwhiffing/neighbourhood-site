957f74b3f5235514fe556b25dcc83d68fd9c2427
Daniel Whiffing <danielwhiffing@gmail.com>
2021-05-04 14:01:51 -0400

    Updates yarn lock

--- artifacts/react-static-templates.js
+++ artifacts/react-static-templates.js
@@ -6,9 +6,9 @@ export default {
   '__react_static_root__/src/pages/404.js': require('__react_static_root__/src/pages/404.js').default,
 '__react_static_root__/src/pages/about.js': require('__react_static_root__/src/pages/about.js').default,
 '__react_static_root__/src/pages/equipment.js': require('__react_static_root__/src/pages/equipment.js').default,
+'__react_static_root__/src/pages/expendables.js': require('__react_static_root__/src/pages/expendables.js').default,
 '__react_static_root__/src/pages/feed.js': require('__react_static_root__/src/pages/feed.js').default,
 '__react_static_root__/src/pages/index.js': require('__react_static_root__/src/pages/index.js').default,
-'__react_static_root__/src/pages/shop.js': require('__react_static_root__/src/pages/shop.js').default,
 '__react_static_root__/src/pages/studios.js': require('__react_static_root__/src/pages/studios.js').default
 }
 
