74750a1a1776e223315493089bf30b804962e91e
kiko <kiko@timur.red>
2021-07-14 13:48:27 -0400

    add link underlines underlines + Google Analytics

--- src/components/Head.js
+++ src/components/Head.js
@@ -3,26 +3,34 @@ import Helmet from 'react-helmet'
 import { useSiteData } from 'react-static'
 
 export const Head = ({ route }) => {
-  const { seoFields } = useSiteData()
-  const { description, title } = seoFields.find(
-    (field) => field.route === route,
-  ) || { title: 'Neighbourhood', description: 'Neighbourhood Studios' }
+	const { seoFields } = useSiteData()
+	const { description, title } = seoFields.find(
+		(field) => field.route === route,
+	) || { title: 'Neighbourhood', description: 'Neighbourhood Studios' }
 
-  return (
-    <Helmet>
-      <title>{title}</title>
-      <meta name="description" cotent={description} />
-      <meta property="og:title" content={title} />
-      <meta property="og:site_name" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:type" content="website" />
-      <meta property="og:locale" content="en_US" />
-      <meta
-        property="og:image"
-        content={'https://neighbourhoodstudios.com/socialshare.jpeg'}
-      />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-    </Helmet>
-  )
+	return (
+		<Helmet>
+			<title>{title}</title>
+			<meta name="description" cotent={description} />
+			<meta property="og:title" content={title} />
+			<meta property="og:site_name" content={title} />
+			<meta property="og:description" content={description} />
+			<meta property="og:type" content="website" />
+			<meta property="og:locale" content="en_US" />
+			<meta
+				property="og:image"
+				content={'https://neighbourhoodstudios.com/socialshare.jpeg'}
+			/>
+			<meta name="twitter:title" content={title} />
+			<meta name="twitter:description" content={description} />
+
+			<script async src='https://www.google-analytics.com/analytics.js' />
+			<script>{`
+							window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+							ga('create', 'UA-175490180-1', 'auto');
+							ga('send', 'pageview');
+							`}
+			</script>
+		</Helmet>
+	)
 }
